4951b0a70a7f8229a548a540d169897fafbc6c4c
unknown <mohammedamin.ababneh@gmail.com>
2022-04-03 15:39:44 +0300

    main

--- Quiz website/Signin/scriptName.js
+++ Quiz website/Signin/scriptName.js
@@ -1,11 +1,14 @@
 sign_up_buttton = document.getElementById("signUp");
 sign_in_buttton = document.getElementById("signIn");
 container = document.getElementById("container");
+body = document.querySelector("body");
 
 sign_up_buttton.addEventListener("click" , function(){
     container.classList.add("active");
+    body.classList.add("active");
 })
 
 sign_in_buttton.addEventListener("click" , function(){
     container.classList.remove("active");
+    body.classList.remove("active");
 })
